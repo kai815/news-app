c2b192207969a930184bef69803f422ba67b2cf5
kai815 <h.kaizuka815@gmail.com>
2021-05-30 19:38:48 +0900

    グレーの箱を表示

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -9,13 +9,20 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
 });
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello React Native!</Text>
-      <StatusBar style="auto" />
+      <View
+        style={styles.itemContainer}
+      />
     </View>
   );
 }
